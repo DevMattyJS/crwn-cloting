71663421a9c7bfdd560beaf81c3e0da4ae5699fd
DevMattyJS <matej.sliacky@protonmail.com>
2023-06-11 17:15:09 +0200

    Created sign in form component, finished authentication page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 
 const App = () => {
@@ -12,7 +12,7 @@ const App = () => {
         {/* index means index={true} => same route as a parent, so both elements are rendered on '/' path */}
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,16 @@
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.styles.scss";
+
+//TODO try to do this component (functionality) by myself
+const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;
